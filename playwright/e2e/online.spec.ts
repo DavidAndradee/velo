bfb8c58af45c68d5ed23979881800de247d9758b
David Andrade <david1999andrade@gmail.com>
2026-06-11 15:23:33 -0300

    vercel configs

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test('App deve estar online', async ({ page }) => {
-    await page.goto('http://localhost:5173/', {timeout: 50000});
+    await page.goto('https://velo-andra-test.vercel.app/', {timeout: 50000});
     await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
 })
